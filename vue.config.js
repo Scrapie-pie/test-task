0ecee5f7db4a0e0174b1cbbaff28fbc13d98f92f
Scrapie-pie <neontec2@gmail.com>
2022-10-06 03:09:55 +0300

    update

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/test-task/'
-    : '/',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/test-task/'
+  //   : '/',
+  publicPath: '',
   transpileDependencies: true,
   chainWebpack: config => {
     config.module.rules.delete("svg");
